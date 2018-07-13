ed726e6f26c88636873822128c91a0478f6cb98d
Brian Intile <brianintile@gmail.com>
2018-07-13 15:11:40 -0400

    Add RestartKey script

---
+++ Assets/Scripts/Debug/RestartKey.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Controller
+{
+    public class RestartKey : MonoBehaviour
+    {
+        [SerializeField]
+        private KeyCode restartKey = KeyCode.R;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(restartKey))
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
