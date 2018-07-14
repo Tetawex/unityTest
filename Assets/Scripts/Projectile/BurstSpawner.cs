e6f1def84a2b928d9cec2a4e3d50c3162b97f253
Brian Intile <brianintile@gmail.com>
2018-07-14 11:21:26 -0400

    Add burst projectile

---
+++ Assets/Scripts/Projectile/BurstSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Util;
+using Assets.Scripts.Controller;
+
+public class BurstSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject shotPrefab;
+    [SerializeField]
+    private int shotCount;
+    [SerializeField]
+    private float timeBetweenShots;
+
+    private int shotsFired;
+    private float shotTimer;
+    private Vector3 direction;
+
+    void Start ()
+    {
+        direction = Utils.getSingleton<PlayerController>().transform.position - transform.position;
+        direction = new Vector3(direction.x, 0f, direction.z);
+
+        fireShot();
+        shotsFired++;
+	}
+	
+	void Update ()
+    {
+        if (shotsFired >= shotCount)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        shotTimer += Time.deltaTime;
+        if (shotTimer >= timeBetweenShots)
+        {
+            fireShot();
+            shotTimer -= timeBetweenShots;
+            shotsFired++;
+        }
+	}
+
+    void fireShot()
+    {
+        Instantiate(shotPrefab, transform.position, Quaternion.identity).GetComponent<MoveRigidbodyTowards>().Direction = direction;
+    }
+}
