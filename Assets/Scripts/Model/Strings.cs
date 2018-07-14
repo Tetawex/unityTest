8de922bd0c3020d3441338d39e7d6f737434ef58
Tetawex <tetawex@gmail.com>
2018-07-14 16:14:42 +0300

    Add a popup level complete window

---
+++ Assets/Scripts/Model/Strings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Assets.Scripts.Model
+{
+    //I have no idea how to make string resources for Unity ui so...
+    public static class Strings
+    {
+        public static String LevelCompletedFailure = "YOU DIED";
+        public static String LevelCompletedSuccess = "Stage Clear";
+    }
+}
---
+++ Assets/Scripts/UI/PlayMenuController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+namespace Assets.Scripts.UI
+{
+    public class PlayMenuController : MonoBehaviour
+    {
+
+        private Button nextButton, restartButton, quitButton;
+        private Text levelCompletedText;
+        private GameObject levelCompleteWindow;
+
+        // Use this for initialization
+        void Start()
+        {
+            BindViews();
+            BindListeners();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        //Init
+        private void BindViews()
+        {
+            try
+            {
+                nextButton = GameObject.Find("NextButton").GetComponent<Button>();
+                restartButton = GameObject.Find("RestartButton").GetComponent<Button>();
+                quitButton = GameObject.Find("QuitButton").GetComponent<Button>();
+                levelCompletedText = GameObject.Find("LevelCompletedText").GetComponent<Text>();
+            }
+            catch
+            {
+                Debug.LogError("Failed to bind views");
+            }
+        }
+        private void BindListeners()
+        {
+            restartButton.onClick.AddListener(() => { SceneManager.LoadScene(SceneManager.GetActiveScene().name); });
+            quitButton.onClick.AddListener(() => { SceneManager.LoadScene("MainMenu", LoadSceneMode.Single); });
+        }
+        //...
+        public void SetLevelCompletedWindowVisibility(bool visibility = true)
+        {
+            levelCompleteWindow.SetActive(visibility);
+        }
+        public void SetToggleUIVisibility(bool visibility = true)
+        {
+            gameObject.SetActive(visibility);
+        }
+        public void ShowLevelCompletedWindow(bool win)
+        {
+            if (!win)
+            {
+                nextButton.gameObject.SetActive(false);
+            }
+        }
+    }
+}
