bf83999f5402b1d4763c1d24c414938825619ac8
Tetawex <tetawex@gmail.com>
2018-07-14 13:40:54 +0300

    Add non-functional win/loss stage ui

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+namespace Assets.Scripts.UI
+{
+    public class MainMenuController : MonoBehaviour
+    {
+
+        private Button playButton, settingsButton, quitButton;
+
+        // Use this for initialization
+        void Start()
+        {
+            BindViews();
+            BindListeners();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void BindViews()
+        {
+            try
+            {
+                playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+                settingsButton = GameObject.Find("SettingsButton").GetComponent<Button>();
+                quitButton = GameObject.Find("QuitButton").GetComponent<Button>();
+            }
+            catch
+            {
+                Debug.LogError("Failed to bind Views");
+            }
+        }
+        private void BindListeners()
+        {
+            playButton.onClick.AddListener(() => { SceneManager.LoadScene("OldScene", LoadSceneMode.Single); });
+            quitButton.onClick.AddListener(() => { Application.Quit(); });
+        }
+    }
+}
