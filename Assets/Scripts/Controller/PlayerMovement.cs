a9246a18c2bdc1fd09d1c4af0fa6509f709f0459
Brian Intile <brianintile@gmail.com>
2018-07-13 20:39:18 -0400

    Add player strafing

---
+++ Assets/Scripts/Controller/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+    [SerializeField]
+    private Vector2 xBounds;
+    
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        float direction = 0f;
+        if (Input.GetKey(KeyCode.A))
+            direction -= 1f;
+        if (Input.GetKey(KeyCode.D))
+            direction += 1f;
+        if (direction != 0f)
+        {
+            var newX = transform.position.x + direction * moveSpeed * Time.deltaTime;
+            newX = Mathf.Clamp(newX, xBounds.x, xBounds.y);
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+    }
+}
