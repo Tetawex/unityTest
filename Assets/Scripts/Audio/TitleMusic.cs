e687efeed73f9de9e2ef933563c0c8d56be8267d
Brian Intile <brianintile@gmail.com>
2018-07-15 13:20:28 -0400

    Add TitleMusic player

---
+++ Assets/Scripts/Audio/TitleMusic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMusic : MonoBehaviour
+{
+    public static TitleMusic instance;
+    
+	void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+    }
+	
+	void Update()
+    {
+        var sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName.StartsWith("Level", System.StringComparison.OrdinalIgnoreCase)
+            && sceneName.Length == 6)
+        {
+            instance = null;
+            Destroy(gameObject);
+        }
+
+	}
+}
